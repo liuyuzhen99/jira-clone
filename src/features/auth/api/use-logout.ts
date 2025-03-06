beef3ee3d99fb13701b5c62f9c28442d8bdf4b38
liuyuzhen99 <liuyuzhenbmjt@163.com>
2025-03-07 07:04:28 +0800

    member function

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -22,6 +22,7 @@ export const useLogout = () => {
       toast.success("Logged out");
       rounter.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
     onError: () => {
       toast.error("Failed to log out");
